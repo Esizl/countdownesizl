06e191c64bacccb447a0dd4b026f26a42a10915d
Esizl <63180275+Esizl@users.noreply.github.com>
2021-06-21 06:39:07 +0000

    fixed port

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/index.js'))
 
-const port = require('esizluuid')(4, '123456789')
+const port = process.env.PORT || require('esizluuid')(4, '123456789')
 app.listen(port, () => console.log(port))
